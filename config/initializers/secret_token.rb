b825c4dd10c538c19ad7808b02f7dff40797a3fb
Patrick <patrick.lighthall@gmail.com>
2013-01-28 15:44:51 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plite::Application.config.secret_token = '9391170ee445702cd085aaac50e30c8924696429ab6c1025c393e0f2d40ec48075d02b4fbb22a1d9563a4356b3dfc02b8fc01d62f6cd7b31c26c1bb7c054cccb'
---
+++ config/initializers/site_constants.rb
@@ -0,0 +1,4 @@
+SITE_NAME = 'Patrick Lighthall'
+SITE_ADDRESS1 = '210 N Kalamazoo Ave'
+SITE_ADDRESS2 = ' Marshall, MI 49068'
+SITE_PHONE = '269.781.0689'
